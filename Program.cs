6a1c240aa8972bcf11c8d9d06928ab9a2f3275df
cesargtrr <163595481+cesargtrr@users.noreply.github.com>
2024-07-12 15:49:39 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+namespace Projeto_Web_Lh_Pets_Alunos;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.MapGet("/", () => " Projeto Web-LH Pets versao1!");
+
+        app.UseStaticFiles();
+        app.MapGet("/index", (HttpContext contexto)=>{
+            contexto.Response.Redirect("index.html", false);
+        });
+
+        app.Run();
+    }
+}
